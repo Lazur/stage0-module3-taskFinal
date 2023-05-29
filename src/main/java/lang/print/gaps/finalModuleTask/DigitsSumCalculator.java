c55b04e7a0df5dbf8491bb04e036d21e2c68339f
Kyrylo_Popov <kyrylo_popov@epam.com>
2023-05-29 14:38:04 +0300

    Module 3 Final task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        number = Math.abs(number); // Ensure number is positive
+        
+        int sum = 0;
+        
+        while (number > 0) {
+            int digit = number % 10;  // Extract the last digit
+            sum += digit;  // Add the digit to the sum
+            number /= 10;  // Remove the last digit from the number
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int digit1 = number / 100;    // Extract the hundreds digit
+        int digit2 = (number / 10) % 10;    // Extract the tens digit
+        int digit3 = number % 10;    // Extract the units digit
+
+        System.out.println(digit3 * 100 + digit2 * 10 + digit1);
     }
 }
